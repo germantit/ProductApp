6d1dfd5731048879fab360c56829c21e7f136243
German Titov <steammy98@mail.ru>
2021-10-07 18:12:09 +0400

    added functionality

--- app/src/main/java/com/example/productapp/data/ProductListRepositoryImpl.kt
+++ app/src/main/java/com/example/productapp/data/ProductListRepositoryImpl.kt
@@ -1,5 +1,7 @@
 package com.example.productapp.data
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.example.productapp.domain.ProductItem
 import com.example.productapp.domain.ProductListRepository
 import java.lang.RuntimeException
@@ -7,19 +9,28 @@ import java.lang.RuntimeException
 object ProductListRepositoryImpl: ProductListRepository {
 
     private val productList = mutableListOf<ProductItem>()
+    private val productListLD = MutableLiveData<List<ProductItem>>()
 
     private var autoIncrementId = 0
+
+    init {
+        for (i in 0 until 10) {
+            val item = ProductItem("Name$i", i)
+            addProduct(item)
+        }
+    }
 
     override fun addProduct(productItem: ProductItem) {
         if (productItem.id == ProductItem.UNDEFINED_ID){
             productItem.id = autoIncrementId++
         }
-        productItem.id = autoIncrementId++
         productList.add(productItem)
+        updateList()
     }
 
     override fun deleteProductItem(productItem: ProductItem) {
         productList.remove(productItem)
+        updateList()
     }
 
     override fun editProductItem(productItem: ProductItem) {
@@ -33,7 +44,11 @@ object ProductListRepositoryImpl: ProductListRepository {
             it.id == productItemId } ?: throw RuntimeException("Element with id $productItemId not found")
     }
 
-    override fun getProductList(): List<ProductItem> {
-        return productList.toList()
+    override fun getProductList(): LiveData<List<ProductItem>> {
+        return productListLD
+    }
+
+    private fun updateList() {
+        productListLD.value = productList.toList()
     }
 }
