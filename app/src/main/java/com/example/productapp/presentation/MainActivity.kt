d52c16f959cfdb2ff6320e17f542cafbf3a1ff11
German Titov <steammy98@mail.ru>
2021-10-08 12:36:36 +0400

    add ProductListFragment

--- app/src/main/java/com/example/productapp/presentation/MainActivity.kt
+++ app/src/main/java/com/example/productapp/presentation/MainActivity.kt
@@ -9,27 +9,8 @@ import com.example.productapp.domain.ProductItem
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var viewModel: ProductViewModel
-    private lateinit var adapter: ProductListAdapter
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setupRecyclerView()
-        viewModel = ViewModelProvider(this).get(ProductViewModel::class.java)
-        viewModel.productList.observe(this) {
-            adapter.productList = it
-        }
-    }
-
-    private fun setupRecyclerView() {
-        val rvProductList = findViewById<RecyclerView>(R.id.rv_product_list)
-        adapter = ProductListAdapter()
-        rvProductList.adapter = adapter
-        adapter.onProductLongClickListener = object : ProductListAdapter.OnProductLongClickListener {
-            override fun onProductLongClick(productItem: ProductItem) {
-                viewModel.deleteProductItem(productItem)
-            }
-        }
     }
 }
