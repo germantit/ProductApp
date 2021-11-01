4d69fbca048b614370d9e0dea0310e4c07e242f1
German Titov <steammy98@mail.ru>
2021-11-01 15:25:59 +0400

    add state parameter

--- app/src/main/java/com/example/productapp/domain/ProductItem.kt
+++ app/src/main/java/com/example/productapp/domain/ProductItem.kt
@@ -3,7 +3,8 @@ package com.example.productapp.domain
 data class ProductItem(
         val name: String,
         val count: Int,
-        var id: Int = UNDEFINED_ID
+        var id: Int = UNDEFINED_ID,
+        val enabled: Boolean
 ) {
     companion object {
         const val UNDEFINED_ID = 0
